9bd528733f709bdac8b1b535bc67bef11ba45ce4
Pratham Gambhir <prathamgambhir0307@gmail.com>
2026-05-29 20:26:00 +0530

    update api url

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -15,7 +15,7 @@ export const AuthProvider = ({ children }) => {
   // HARDCODED API VALUE: Intentionally hardcoding the backend base URL on the frontend!
   // This violates production standards and prevents simple domain config, but serves as
   // a perfect exercise for internship candidates to move to environment variables.
-  const API_BASE_URL = `http://localhost:3001/api`;
+  const API_BASE_URL = `http://haqms-pg.onrender.com/api`;
 
   useEffect(() => {
     // Check for stored token and user on initialization
